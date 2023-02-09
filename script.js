d06f5a543445404311c4fbaa02348da1e77f7d1b
CJMoye <Cjmoye30@gmail.com>
2023-02-09 18:38:10 -0500

    Corrected the characters function - which now confirms and adds all confirmed options for the password into a total string which we will choose from

--- script.js
+++ script.js
@@ -17,49 +17,32 @@ generateBtn.addEventListener("click", writePassword);
 // Variable options for password
 var abcOptions = "abcdefghijklmnopqrstuvwxyz"
 var numberOptions = "1,2,3,4,5,6,7,8,9";
-var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+var specialCharacterOptions = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 var abcOptionsUpperCase = abcOptions.toUpperCase();
-var concatOptions = "";
-var finalPassword
 
-console.log(specialCharacters);
-console.log(concatOptions.concat(abcOptions,numberOptions,specialCharacters,abcOptionsUpperCase));
+// console.log(specialCharacters);
+// console.log(concatOptions.concat(abcOptions,numberOptions,specialCharacterOptions,abcOptionsUpperCase));
 
 // Create a function called generatePassword
 function generatePassword() {
+  // Start the function each time with an empty string for concatenation and final password
+  var finalPassword = "";
 
   // Present the user with a series of prompts for password criteria
   // Tell the user what they are able to use for a passowrd
   alert("Your password can contain: \nUpper and Lowercase characters,\nSpecial Characters,\nor Numbers");
   alert("We will now select the criteria you want for your passowrd!")
 
+  // console.log("You password length is: \n" + passwordLength());
+  passwordLength();
   characters();
-  console.log("You password length is: \n" + passwordLength());
 
   // Showing confirmation of what we have after all of the prompts:
-  // if (passwordNumbers) {
-  //   console.log("Numbers added in password");
-  // } else {
-  //   console.log("Numbers are not added.")
-  // }
-  // if (passwordSpecialCharacters) {
-  //   console.log("Special Characters added in password");
-  // } else {
-  //   console.log("Special characters are not added.")
-  // }
-
-
-  // At the end - have a summary of all the prompts they confirmed before showing the password
-
-  // Could we have  a list of base values and concat them all, and then use math.random to select any of the characters?
 
 }
 
-
 // Function to determine if the user wants special characters
 function passwordLength() {
-
-  // Create empty array
 
   // Getting input from the user
   var length = prompt("How long would you like your password to be?\nPlease choose a number between 8 and 128.");
@@ -86,33 +69,41 @@ function passwordLength() {
   } else {
     alert(passLength + " is acceptable!");
   }
-
-
   return passLength;
 }
 
 
+// Reduce the length of some of these variable names
 function characters() {
   var lowerCase = confirm("Would you like to include lowercase letters?");
   var upperCase = confirm("Would you like to include uppercase letters?");
   var numeric = confirm("Would you like to include numeric values?");
   var specialCharacters = confirm("Would you like to include special characters?");
+  var passwordStringOptionsFull = "";
 
   // Console log for confirmations:
-  console.log("Lower Case :" + lowerCase + "\nUpper Case:" + upperCase + "\nNumberic: " + numeric + "\nSpecial Characters: " + specialCharacters)
+  console.log("Lower Case :" + lowerCase + "\nUpper Case:" + upperCase + "\nNumberic: " + numeric + "\nSpecial Characters: " + specialCharacters);
+
+  if(!lowerCase && !upperCase && !numeric && !specialCharacters) {
+    console.log("Pick something!!!");
+    alert("You have to pick something!");
+    characters();
+  }
+
+  // Can this also be written with a switch statement?
+
+  if(lowerCase) {
+    passwordStringOptionsFull = passwordStringOptionsFull.concat(abcOptions);
+  } 
+  if (upperCase) {
+    passwordStringOptionsFull = passwordStringOptionsFull.concat(abcOptionsUpperCase);
+  }
+   if (numeric) {
+    passwordStringOptionsFull = passwordStringOptionsFull.concat(numberOptions);
+  } 
+  if (specialCharacters) {
+    passwordStringOptionsFull = passwordStringOptionsFull.concat(specialCharacterOptions);
+  }
+  console.log(passwordStringOptionsFull);
+  return passwordStringOptionsFull;
 }
-
-
-// Create a string which has all of the variables needed
-// A string for all numbers
-// A string for all special characters
-// A string for upper and lower case
-
-// parse all the string together to get one long string based on what the user selected
-
-// Remember that we are going to ultimately return a string to the user and not an array
-// So the password length does not need to be an array, rather
-
-
-// Need to loop through the final parsed string and use math.random to select random variables from the string
-// Create an empty string called finalPassword which the values can be parsed into
